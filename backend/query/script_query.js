63af465b114fffcf5c66cd4c2d6c19c52effe7b9
Egi Muhammad <eragilang@gmail.com>
2018-11-05 23:43:35 +0700

    [Backend] Update Query Script
    - LIst Penipu
    - Get Reference Penipu
    - insert Detail
    - Update Blocked Status

--- backend/query/script_query.js
+++ backend/query/script_query.js
@@ -5,6 +5,19 @@ const kon = require('../service/dbset')
     kon("SELECT * from d_summary_penipu order by jumlah_pelapor limit 0,20", callback)
 }
 
+function insertDetail(body,callback){
+   var query = "insert into t_sms_penipu(msisdn_target,msisdn_pelapor,content,date_system,uploader,source) "
+                + "values (${body.msisdn_target},${body.msisdn_pelapor},'${body.content}',NOW(),'${body.uploader}','${body.source}')"
+    kon(query,callback)
+}
 
+function update_reference(body,callback){
+    var query = "update reference_penipu set status = '${body.status},updated_date = NOW(),updated_by='${body.updater} where "
+                + "msisdn=${body.msisdn}"
+}   
 
-module.exports = {getListPenipu}
+function getReference(callback){
+    kon("select * from reference_penipu", callback)
+}
+
+module.exports = {getListPenipu, insertDetail, getReference, update_reference}
