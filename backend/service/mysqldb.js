28d923e5989b9a7d3aec2e2128d16adb4c347a67
Ligar Mugi Syahid <mugisyahid@gmail.com>
2018-11-05 10:52:53 +0700

    connection

---
+++ backend/service/mysqldb.js
@@ -0,0 +1,124 @@
+'use strict'
+
+const mysql = require('mysql')
+const config = require('../config/config_all')
+
+const pool = mysql.createPool({
+  host: config.mysql.host,
+  user: config.mysql.user,
+  password: config.mysql.user,
+  database: config.mysql.database
+})
+
+const pool2 = mysql.createPool({
+  host: config.mysql2.host,
+  user: config.mysql2.user,
+  password: config.mysql2.user,
+  database: config.mysql2.database
+})
+
+
+const pool3 = mysql.createPool({
+  host: config.mysql3.host,
+  user: config.mysql3.user,
+  password: config.mysql3.user,
+  database: config.mysql3.database
+})
+
+let db = (function () {
+  function _query(query, params, callback) {
+    pool.getConnection(function (err, connection) {
+      if (err) {
+        connection.release()
+        callback(null, err)
+        throw err
+      }
+
+      connection.query(query, params, function (err, rows) {
+        connection.release()
+        if (!err) {
+          callback(rows)
+        }
+        else {
+          callback(null, err)
+        }
+
+      })
+
+      connection.on('error', function (err) {
+        connection.release()
+        callback(null, err)
+        throw err
+      })
+    })
+  }
+  return {
+    query: _query
+  }
+})()
+
+let db2 = (function () {
+  function _query(query, params, callback) {
+    pool2.getConnection(function (err, connection) {
+      if (err) {
+        connection.release()
+        callback(null, err)
+        throw err
+      }
+
+      connection.query(query, params, function (err, rows) {
+        connection.release()
+        if (!err) {
+          callback(rows)
+        }
+        else {
+          callback(null, err)
+        }
+
+      })
+
+      connection.on('error', function (err) {
+        connection.release()
+        callback(null, err)
+        throw err
+      })
+    })
+  }
+  return {
+    query: _query
+  }
+})()
+
+let db3 = (function () {
+  function _query(query, params, callback) {
+    pool3.getConnection(function (err, connection) {
+      if (err) {
+        connection.release()
+        callback(null, err)
+        throw err
+      }
+
+      connection.query(query, params, function (err, rows) {
+        connection.release()
+        if (!err) {
+          callback(rows)
+        }
+        else {
+          callback(null, err)
+        }
+
+      })
+
+      connection.on('error', function (err) {
+        connection.release()
+        callback(null, err)
+        throw err
+      })
+    })
+  }
+  return {
+    query: _query
+  }
+})()
+
+module.exports = {db, db2, db3}
