93714484c5acd3d10c55065006e51c1796944d19
Ligar Mugi Syahid <mugisyahid@gmail.com>
2018-11-06 20:21:17 +0700

    [BACKEND] Better Connection

--- backend/routes/api/penipu.js
+++ backend/routes/api/penipu.js
@@ -1,9 +1,10 @@
 'use strict'
 
 const router = require('express').Router()
+const query = require('../../query/penipu')
 
 router.get('/', (req, res) => {
-    query.getListPenipu((err, r) => {
+    query.getPenipu(req.query.f, req.query.t, (err, r) => {
         res.send(r)
     })
 })
--- backend/service/dbset.js
+++ backend/service/dbset.js
@@ -1,46 +1,29 @@
-var mysql = require('mysql');
+var mysql = require('mysql')
 const conf = require('../config/config_all')
 const logger = require('../service/logger')
 
-var con = mysql.createConnection({
-    host: conf.mysql.host,
-    user: conf.mysql.user,
-    database: conf.mysql.database
-});
-
-var con2 = mysql.createConnection({
-    host: conf.mysql2.host,
-    user: conf.mysql2.user,
-    database: conf.mysql2.database
-});
-
 
 function ekse(query, callback) {
-    logger.debug(query)
-    con.connect(function (err) {
-        if (err) throw err;
-        con.query(query, function (err, result, fields) {
-            logger.debug(result)
-            con.end()
-            if (err) {
-                callback(err, null)
-            } else { callback(null, result)  }
-        });
-    });
+    let connection = mysql.createConnection(conf.mysql)
+    connection.connect()
+    connection.query(query, function (err, result) {
+        if (err) throw callback(err, null)
+        connection.end()
+        callback(null, result)
+    })
 }
 
 function ekse2(query, callback) {
-    console.log(query)
-    con2.connect(function (err) {
-        if (err) throw err;
-        con2.query(query, function (err, result, fields) {
-            logger.debug(result)
-            con2.end()
-            if (err) {
-                callback(err, null)
-            } else { callback(null, result);  }
-        });
-    });
+    let connection = mysql.createConnection(conf.mysql2)
+    connection.connect()
+    connection.query(query, function (err, result) {
+        if (err) throw callback(err, null)
+        connection.end()
+        callback(null, result)
+    })
 }
 
-module.exports = {ekse, ekse2}
+module.exports = {
+    ekse,
+    ekse2
+}
