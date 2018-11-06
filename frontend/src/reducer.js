2c1a6fdfb278ad867c78657b4011abcb7a4642a3
Ligar Mugi Syahid <mugisyahid@gmail.com>
2018-11-06 15:35:08 +0700

    [FRONTEND] Cleanup code. define api

--- frontend/src/reducer.js
+++ frontend/src/reducer.js
@@ -2,13 +2,9 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import common from "./reducers/common";
 import auth from "./reducers/auth";
-import ticket from "./reducers/tickets";
-import user from "./reducers/users";
 
 export default combineReducers({
   auth,
   common,
-  ticket,
-  user,
   router: routerReducer
 });
--- frontend/src/views/Dashboard/Dashboard.jsx
+++ frontend/src/views/Dashboard/Dashboard.jsx
@@ -1,8 +1,6 @@
-/* eslint-disable */
+ /* eslint-disable */
 import React, { Component } from "react";
 import { Redirect, withRouter} from 'react-router-dom';
-import { Grid, Row, Col } from "react-bootstrap";
-import { StatsCard } from "../../components/StatsCard/StatsCard.jsx";
 import { connect } from 'react-redux';
 import agent from '../../agent';
 
@@ -12,8 +10,7 @@ import {
 } from '../../constants/actionTypes';
 
 const mapStateToProps = state => ({
-  ...state,
-  stats: state.ticket.stats
+  ...state
 })
 
 const mapDispatchToProps = dispatch => ({
@@ -28,41 +25,6 @@ const mapDispatchToProps = dispatch => ({
 class Dashboard extends Component {
 
   componentWillMount() {
-    const user = JSON.parse(window.localStorage.getItem('user'))
-
-    if (user.PRIVILEGES_ROLES === 'Creator') {
-      this.props.onLoad(Promise.all([
-        agent.Ticket.countTicket(user.ID, 'Created'),
-        agent.Ticket.countTicket(user.ID, 'Approved'),
-        agent.Ticket.countTicket(user.ID, 'Rejected'),
-        agent.Ticket.countTicket(user.ID, 'In Progress'),
-        agent.Ticket.countTicket(user.ID, 'Pending review'),
-        agent.Ticket.countTicket(user.ID, 'Done'),
-        agent.Ticket.countTicket(user.ID, 'All'),
-        agent.Ticket.countAll()]))
-    } else if (user.PRIVILEGES_ROLES === 'Approver') {
-      this.props.onLoad(Promise.all([
-        agent.Ticket.countTicketApprover(user.ID, 'Created'),
-        agent.Ticket.countTicketApprover(user.ID, 'Approved'),
-        agent.Ticket.countTicketApprover(user.ID, 'Rejected'),
-        agent.Ticket.countTicketApprover(user.ID, 'In Progress'),
-        agent.Ticket.countTicketApprover(user.ID, 'Pending review'),
-        agent.Ticket.countTicketApprover(user.ID, 'Done'),
-        agent.Ticket.countTicketApprover(user.ID, 'All')]))
-    } else if (user.PRIVILEGES_ROLES === 'Executor') {
-      this.props.onLoad(Promise.all([agent.Ticket.countTicketExecutor(user.ID, 'Approved'),
-      agent.Ticket.countTicketExecutor(user.ID, 'In Progress'),
-      agent.Ticket.countTicketExecutor(user.ID, 'Pending review'),
-      agent.Ticket.countTicketExecutor(user.ID, 'Done'),
-      agent.Ticket.countTicketExecutor(user.ID, 'All'),
-      agent.Ticket.countAll()]))
-    }
-    else {
-      this.props.onLoad(Promise.all([agent.Ticket.countTicketAdmin(user.ID, 'Created'),
-      agent.Ticket.countTicketAdmin(user.ID, 'Approved'),
-      agent.Ticket.countTicketAdmin(user.ID, 'In Progress'),
-      agent.Ticket.countTicketAdmin(user.ID, 'Done')]))
-    }
   }
   componentWillUnmount() {
     this.props.onUnload();
