24b75460221393b65f14b5aca1025caba54cfb8d
Egi Muhammad <eragilang@gmail.com>
2018-11-07 13:52:33 +0700

    [BACKEND] Latest
    - Update Status
    - Detail Laporan

--- backend/query/reference.js
+++ backend/query/reference.js
@@ -5,10 +5,18 @@ function getReference(msisdn, callback) {
     kon.ekse(`select * from reference_penipu where msisdn = '${msisdn}' limit 1`, callback)
 }
 
-function updateReference(param, callback) {
-    const query = `update reference_penipu set status = '${param.status}', updated_date = now(), updated_by = '${param.updater}' where msisdn = '${param.msisdn}'`
-    logger.debug(query)
-    kon.ekse(query, callback)
+function updateReference(p, callback) {
+    p.forEach(param => {
+        const query = `delete reference_penipu where msisdn = '${param.msisdn}'`
+        let query2 = `insert into reference_penipu values('${param.msisdn}','${param.status}','${param.updater}',NOW(),1)`
+        logger.debug(query)
+        kon.ekse(query, (e,r) => {
+            if(e){ callback(e,null)}
+            else{ kon.ekse(query2,callback)}
+        })
+    });
+   
+    
 }
 
 module.exports = {
